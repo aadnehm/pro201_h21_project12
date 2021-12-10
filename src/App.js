db2503776c169f8074e8f1bf3f62a674f7feca6a
Terje <terjeslaatto@gmail.com>
2021-12-10 18:15:18 +0100

    routing v1 done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import './App.css';
 //MUI
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 //React
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+//Firebase
+import db from './lib/firebase';
 
 /* Changing default value for secondary color */
 const mainColor = createTheme({
@@ -18,12 +21,33 @@ const mainColor = createTheme({
 });
 
 function App() {
+	//Pulling non-profits from cloud database and storing it in state that gets passed
+	//to SearchNonProfits-page
+	const fetchOrgs = async () => {
+		const response = db.collection('nonprofits');
+		const data = await response.get();
+		data.forEach((element) => {
+			setOrgs((prevValue) => [
+				...prevValue,
+				element.data()
+			]);
+		});
+	};
+	const [
+		orgs,
+		setOrgs
+	] = useState([]);
+
+	useEffect(() => {
+		fetchOrgs();
+	}, []);
+
 	return (
 		<ThemeProvider theme={mainColor}>
 			<div className="App">
 				<Routes>
 					<Route path="/" element={<Login />} />
-					<Route path="/nonprofits" element={<SearchNonProfits />} />
+					<Route path="/nonprofits" element={<SearchNonProfits orgs={orgs} />} />
 				</Routes>
 			</div>
 		</ThemeProvider>
--- src/components/search/SearchNonProfits.js
+++ src/components/search/SearchNonProfits.js
@@ -1,45 +1,41 @@
 import Search from './Search';
 import './Search.css';
-import nonProfitsPosts from './NonProfitsData';
-import React, {  useState } from "react";
-
+import React, { useState } from 'react';
 
 const filterPosts = (nonProfitsPosts, query) => {
-  if (!query) {
-      return nonProfitsPosts;
-  }
+	if (!query) {
+		return nonProfitsPosts;
+	}
 
-  return nonProfitsPosts.filter((post) => {
-      const postName = post.name.toLowerCase();
-      return postName.includes(query);
-  });
+	return nonProfitsPosts.filter((post) => {
+		const postName = post.name.toLowerCase();
+		return postName.includes(query);
+	});
 };
 
-const SearchNonProfits = () => {
-  const [searchQuery, setSearchQuery] = useState('');
-  const filteredPosts = filterPosts(nonProfitsPosts, searchQuery);
+const SearchNonProfits = ({ orgs }) => {
+	const [
+		searchQuery,
+		setSearchQuery
+	] = useState('');
+	const filteredPosts = filterPosts(orgs, searchQuery);
 
-
-
-  return (
-      <div>
-            <Search 
-              searchQuery={searchQuery}
-              setSearchQuery={setSearchQuery}
-            />
-                <div className="cards">
-                    {filteredPosts.map(nonProfitsPosts => (
-                      <div className="card">
-                        <img className="image" src={nonProfitsPosts.img} alt={nonProfitsPosts.name}/>
-                          <div className="cardName" key={nonProfitsPosts.key}>
-                            {nonProfitsPosts.name}
-                            <br />
-                          </div>
-                      </div>
-                    ))}
-                </div>
-      </div>
-  );
-}
+	return (
+		<div>
+			<Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+			<div className="cards">
+				{filteredPosts.map((nonProfitsPosts) => (
+					<div className="card">
+						<img className="image" src={`./img/${nonProfitsPosts.img}`} alt={nonProfitsPosts.name} />
+						<div className="cardName" key={nonProfitsPosts.key}>
+							{nonProfitsPosts.name}
+							<br />
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+};
 
 export default SearchNonProfits;
