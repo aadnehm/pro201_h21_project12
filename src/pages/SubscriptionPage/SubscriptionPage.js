415c3a7cf25ef62a01ee4610421bc540bd9e4694
SigmundGjengedal <sigmund.gjengedal@gmail.com>
2021-12-10 12:19:48 +0100

    container-work in progress

--- src/pages/SubscriptionPage/SubscriptionPage.js
+++ src/pages/SubscriptionPage/SubscriptionPage.js
@@ -6,13 +6,7 @@ const SubscriptionPage = () => {
   return (
     <div className="container">
       <section className="line">
-        <div className="blue-line">
-          <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-        </div>
+        <div className="blue-line"></div>
       </section>
       <SubscriptionCard />
     </div>
