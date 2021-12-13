1644a7272a7f992d7e1ee901ac565d884dc2c360
SigmundGjengedal <sigmund.gjengedal@gmail.com>
2021-12-13 13:00:57 +0100

    fix

--- src/pages/SubscriptionPage/SubscriptionPage.js
+++ src/pages/SubscriptionPage/SubscriptionPage.js
@@ -6,7 +6,11 @@ import "./subscriptionPage.css";
 const SubscriptionPage = () => {
   return (
     <div className="sub-page-container">
+      <div className="circle">
+        <span className="circle-top-right"></span>
+      </div>
       <div className="blue-line"></div>
+
       <div className="heading">
         <h2>
           The payment from subscription goes to Meliora Impact, so we can help
