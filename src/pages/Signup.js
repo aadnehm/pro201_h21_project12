ac3470b94725f79c83fedcf20c0138b4d1d16d23
TerjeSlaatto <Terjeslaatto@gmail.com>
2021-12-13 14:15:43 +0100

    too many requests aut failure

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -12,6 +12,7 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState('');
 
   function handlePassword(e) {
     setPassword(e.target.value);
@@ -20,21 +21,27 @@ const Signup = () => {
     setEmail(e.target.value);
   }
 
+  const handleSignup = async () => {
+     await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        setError(error.message)
+        // ..
+      });
+  }
+
+
   const auth = getAuth();
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-    });
+
+  
   return (
     <>
       <div>
+      {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
         <input label="E-mail" value={email} onChange={handleEmail}></input>
         <input
           label="Passowrd"
@@ -47,7 +54,7 @@ const Signup = () => {
           size="large"
           color="secondary"
           variant="contained"
-          onClick={createUserWithEmailAndPassword}
+          onClick={handleSignup}
         >
           Sign up
         </Button>
