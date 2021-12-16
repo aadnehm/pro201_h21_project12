cb45e265657a7a06999fe1c43a44e3415e72f889
Velpre <veljko_kv@hotmail.com>
2021-12-16 15:15:55 +0100

    finish with firma logo cards

--- src/pages/non-profit/NonProfitMain.js
+++ src/pages/non-profit/NonProfitMain.js
@@ -26,42 +26,110 @@ export default function NonProfit() {
         <h2>Our projects</h2>
 
         <div className="cards-div">
-          <Card onClick={Navigate} className="card">
+          <Card onClick={Navigate} className="card-non-profit">
             <CardContent
               style={{ backgroundImage: "url(./img/non-profit/card-img.png)" }}
               className="card-content"
             ></CardContent>
-            <div className="project-div">
+            <div className="project-div-non-profit">
               <h3>Uganda</h3>
+              <div className="logo-firma-non-profit-img-wrap">
+                <img
+                  class="logo-firma-non-profit-img"
+                  src="./img/non-profit/firma-logo1.png"
+                  alt="logo-firma1"
+                />
+                <img
+                  class="logo-firma-non-profit-img"
+                  src="./img/non-profit/firma-logo2.png"
+                  alt="logo-firma2"
+                />
+                <img
+                  class="logo-firma-non-profit-img"
+                  src="./img/non-profit/firma-logo3.png"
+                  alt="logo-firma3"
+                />
+              </div>
             </div>
           </Card>
-          <Card onClick={Navigate} className="card">
+          <Card onClick={Navigate} className="card-non-profit">
             <CardContent
               className="card-content"
               style={{ backgroundImage: "url(./img/non-profit/card-img1.png)" }}
             ></CardContent>
-            <div className="project-div">
+            <div className="project-div-non-profit">
               <h3>Nepal</h3>
+              <div className="logo-firma-non-profit-img-wrap">
+                <img
+                  class="logo-firma-non-profit-img"
+                  src="./img/non-profit/firma-logo1.png"
+                  alt="logo-firma1"
+                />
+                <img
+                  class="logo-firma-non-profit-img"
+                  src="./img/non-profit/firma-logo2.png"
+                  alt="logo-firma2"
+                />
+                <img
+                  class="logo-firma-non-profit-img"
+                  src="./img/non-profit/firma-logo3.png"
+                  alt="logo-firma3"
+                />
+              </div>
             </div>
           </Card>
-          <Card onClick={Navigate} className="card">
+          <Card onClick={Navigate} className="card-non-profit">
             <CardContent
               className="card-content"
               style={{ backgroundImage: "url(./img/non-profit/card-img2.png)" }}
             ></CardContent>
-            <div className="project-div">
+            <div className="project-div-non-profit">
               <h3>Syria</h3>
+              <div className="logo-firma-non-profit-img-wrap">
+                <img
+                  class="logo-firma-non-profit-img"
+                  src="./img/non-profit/firma-logo1.png"
+                  alt="logo-firma1"
+                />
+                <img
+                  class="logo-firma-non-profit-img"
+                  src="./img/non-profit/firma-logo2.png"
+                  alt="logo-firma2"
+                />
+                <img
+                  class="logo-firma-non-profit-img"
+                  src="./img/non-profit/firma-logo3.png"
+                  alt="logo-firma3"
+                />
+              </div>
             </div>
           </Card>
-          <Card onClick={Navigate} className="card">
+          <Card onClick={Navigate} className="card-non-profit">
             <CardContent
               className="card-content"
               style={{
                 backgroundImage: "url(./img/non-profit/card-img3.jpeg)",
               }}
             ></CardContent>
-            <div className="project-div">
-              <h3>Trygt tilbake til skolen etter pandemien</h3>
+            <div className="project-div-non-profit">
+                <h3>Trygt tilbake til skolen</h3>
+              <div className="logo-firma-non-profit-img-wrap">
+                <img
+                  class="logo-firma-non-profit-img"
+                  src="./img/non-profit/firma-logo1.png"
+                  alt="logo-firma1"
+                />
+                <img
+                  class="logo-firma-non-profit-img"
+                  src="./img/non-profit/firma-logo2.png"
+                  alt="logo-firma2"
+                />
+                <img
+                  class="logo-firma-non-profit-img"
+                  src="./img/non-profit/firma-logo3.png"
+                  alt="logo-firma3"
+                />
+              </div>
             </div>
           </Card>
         </div>
