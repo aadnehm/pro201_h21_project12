0ce86c99a84b6fc0eaa1d5e407e3c98b2c8154e8
aadne <adma007@student.kristiania.no>
2021-12-16 15:43:06 +0100

    starting to fix org code input

--- src/pages/signin-employee/SigninEmployee.js
+++ src/pages/signin-employee/SigninEmployee.js
@@ -1,286 +1,279 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
 //CSS
-import './signinEmployee.css';
+import "./signinEmployee.css";
 //Components
-import PasswordInput from '../../components/Login-Signup/input-fields/PasswordInput';
-import BlankInput from '../../components/Login-Signup/input-fields/BlankInput';
+import PasswordInput from "../../components/Login-Signup/input-fields/PasswordInput";
+import BlankInput from "../../components/Login-Signup/input-fields/BlankInput";
 //MUI
-import { Button, TextField, Box } from '@mui/material/';
+import { Button, TextField, Box } from "@mui/material/";
 //Firebase
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
-import db from '../../lib/firebase';
-import { addDoc, collection } from 'firebase/firestore';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import db from "../../lib/firebase";
+import { addDoc, collection } from "firebase/firestore";
 
 export default function SigninEmployee() {
-	//Navigation
-	const navigate = useNavigate();
+  //Navigation
+  const navigate = useNavigate();
 
-	//Firebase state
-	const [
-		error,
-		setError
-	] = useState('');
-	const [
-		companies,
-		setCompanies
-	] = useState([]);
+  //Firebase state
+  const [error, setError] = useState("");
+  const [companies, setCompanies] = useState([]);
 
-	/* Values Epost & Password & Name */
-	const [
-		code,
-		setCode
-	] = useState();
-	const [
-		name,
-		setName
-	] = useState();
-	const [
-		dataEpost,
-		setDataEpost
-	] = useState();
-	const [
-		dataPassword,
-		setDataPassword
-	] = useState();
-	const [
-		dataPassword1,
-		setDataPassword1
-	] = useState();
+  /* Values Epost & Password & Name */
+  const [code, setCode] = useState();
+  const [code1, setCode1] = useState();
+  const [code2, setCode2] = useState();
+  const [code3, setCode3] = useState();
+  const [name, setName] = useState();
+  const [dataEpost, setDataEpost] = useState();
+  const [dataPassword, setDataPassword] = useState();
+  const [dataPassword1, setDataPassword1] = useState();
 
-	/* State for input validation */
-	const [
-		codeError,
-		setCodeError
-	] = useState(false);
-	const [
-		nameError,
-		setNameError
-	] = useState(false);
-	const [
-		epostError,
-		setEpostError
-	] = useState(false);
-	const [
-		passwordError,
-		setPasswordError
-	] = useState(false);
-	const [
-		passwordError1,
-		setPasswordError1
-	] = useState(false);
+  /* State for input validation */
+  const [codeError, setCodeError] = useState(false);
+  const [nameError, setNameError] = useState(false);
+  const [epostError, setEpostError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordError1, setPasswordError1] = useState(false);
 
-	/* State for helperText */
-	const [
-		codeHelperText,
-		setCodeHelperText
-	] = useState(' ');
-	const [
-		nameHelperText,
-		setNameHelperText
-	] = useState(' ');
-	const [
-		epostHelperText,
-		setEpostHelperText
-	] = useState(' ');
-	const [
-		passwordHelperText,
-		setPasswordHelperText
-	] = useState(' ');
-	const [
-		passwordHelperText1,
-		setPasswordHelperText1
-	] = useState(' ');
+  /* State for helperText */
+  const [codeHelperText, setCodeHelperText] = useState(" ");
+  const [nameHelperText, setNameHelperText] = useState(" ");
+  const [epostHelperText, setEpostHelperText] = useState(" ");
+  const [passwordHelperText, setPasswordHelperText] = useState(" ");
+  const [passwordHelperText1, setPasswordHelperText1] = useState(" ");
 
-	/* Henter data fra child components BlankInput & PasswordInput */
-	function childToParentEpost(e) {
-		const newData = e.target.value;
-		setDataEpost(newData);
-	}
-	function childToParentPassword(e) {
-		const newData = e.target.value;
-		setDataPassword(newData);
-	}
-	function childToParentPassword1(e) {
-		const newData = e.target.value;
-		setDataPassword1(newData);
-	}
+  /* Henter data fra child components BlankInput & PasswordInput */
+  function childToParentEpost(e) {
+    const newData = e.target.value;
+    setDataEpost(newData);
+  }
+  function childToParentPassword(e) {
+    const newData = e.target.value;
+    setDataPassword(newData);
+  }
+  function childToParentPassword1(e) {
+    const newData = e.target.value;
+    setDataPassword1(newData);
+  }
 
-	function childToParentName(e) {
-		const newData = e.target.value;
-		setName(newData);
-	}
+  function childToParentName(e) {
+    const newData = e.target.value;
+    setName(newData);
+  }
 
-	/* Input validation function */
-	function validateInputs() {
-		let needsMoreValidation = true;
-		if (code) {
-			setCodeError(false);
-			setCodeHelperText(' ');
-		}
-		else {
-			setCodeError(true);
-			setCodeHelperText('Enter org code');
-			needsMoreValidation = false;
-		}
-		if (name) {
-			setNameError(false);
-			setNameHelperText(' ');
-		}
-		else {
-			setNameError(true);
-			setNameHelperText('Please enter your name');
-			needsMoreValidation = false;
-		}
+  /* Input validation function */
+  function validateInputs() {
+    let needsMoreValidation = true;
+    if (code) {
+      setCodeError(false);
+      setCodeHelperText(" ");
+    } else {
+      setCodeError(true);
+      setCodeHelperText("Enter org code");
+      needsMoreValidation = false;
+    }
+    if (name) {
+      setNameError(false);
+      setNameHelperText(" ");
+    } else {
+      setNameError(true);
+      setNameHelperText("Please enter your name");
+      needsMoreValidation = false;
+    }
 
-		if (typeof dataEpost === 'undefined' || !dataEpost.includes('@' && '.')) {
-			setEpostError(true);
-			setEpostHelperText('Please use valid e-mail');
-			needsMoreValidation = false;
-		}
-		else if (dataEpost.includes('@' && '.') && dataEpost.length >= 8) {
-			setEpostError(false);
-			setEpostHelperText(' ');
-		}
+    if (typeof dataEpost === "undefined" || !dataEpost.includes("@" && ".")) {
+      setEpostError(true);
+      setEpostHelperText("Please use valid e-mail");
+      needsMoreValidation = false;
+    } else if (dataEpost.includes("@" && ".") && dataEpost.length >= 8) {
+      setEpostError(false);
+      setEpostHelperText(" ");
+    }
 
-		if (typeof dataPassword === 'undefined' || dataPassword.length < 6) {
-			setPasswordError(true);
-			setPasswordHelperText('6 characters or more');
-			needsMoreValidation = false;
-		}
-		else {
-			setPasswordError(false);
-			setPasswordHelperText(' ');
-		}
+    if (typeof dataPassword === "undefined" || dataPassword.length < 6) {
+      setPasswordError(true);
+      setPasswordHelperText("6 characters or more");
+      needsMoreValidation = false;
+    } else {
+      setPasswordError(false);
+      setPasswordHelperText(" ");
+    }
 
-		if (typeof dataPassword1 === 'undefined' || dataPassword1 !== dataPassword) {
-			setPasswordError1(true);
-			setPasswordHelperText1('Passwords dont match');
-			needsMoreValidation = false;
-		}
-		else {
-			setPasswordError1(false);
-			setPasswordHelperText1(' ');
-		}
-		return needsMoreValidation;
-	}
-	//Fetching companies
-	const fetchCompanies = async () => {
-		const response = db.collection('companies');
-		const data = await response.get();
-		data.forEach((element) => {
-			setCompanies((prevState) => [
-				...prevState,
-				element.data()
-			]);
-		});
-	};
+    if (
+      typeof dataPassword1 === "undefined" ||
+      dataPassword1 !== dataPassword
+    ) {
+      setPasswordError1(true);
+      setPasswordHelperText1("Passwords dont match");
+      needsMoreValidation = false;
+    } else {
+      setPasswordError1(false);
+      setPasswordHelperText1(" ");
+    }
+    return needsMoreValidation;
+  }
+  //Fetching companies
+  const fetchCompanies = async () => {
+    const response = db.collection("companies");
+    const data = await response.get();
+    data.forEach((element) => {
+      setCompanies((prevState) => [...prevState, element.data()]);
+    });
+  };
 
-	/* HandleLogin function */
+  /* HandleLogin function */
 
-	const handleLogin = async () => {
-		const auth = getAuth();
-		const temp = companies.filter((element) => element.firmakode === code);
-		if (validateInputs()) {
-			if (temp.length == 1) {
-				await createUserWithEmailAndPassword(auth, dataEpost, dataPassword, code)
-					.then((userCredential) => {
-						const user = userCredential.user;
+  const handleLogin = async () => {
+    const auth = getAuth();
+    const temp = companies.filter((element) => element.firmakode === code);
+    if (validateInputs()) {
+      if (temp.length == 1) {
+        await createUserWithEmailAndPassword(
+          auth,
+          dataEpost,
+          dataPassword,
+          code
+        )
+          .then((userCredential) => {
+            const user = userCredential.user;
 
-						alert(user.email + ' created');
-						const docRef = addDoc(collection(db, 'users'), {
-							navn      : `${name}`,
-							epost     : `${dataEpost}`,
-							firmakode : `${code}`,
-							admin     : `${false}`,
-							aktiv     : `${true}`
-						});
-						setCompanies([]);
-						navigate('/nonprofits');
-					})
-					.catch((error) => {
-						setError(error.message);
-					});
-			}
-			else {
-				setError('Finner ikke registrert firma med denne firmakoden. Kontakt ansvarlig i firmaet ditt');
-				alert('Finner ikke registrert firma med denne firmakoden. Kontakt ansvarlig i firmaet ditt');
-			}
-		}
-	};
+            alert(user.email + " created");
+            const docRef = addDoc(collection(db, "users"), {
+              navn: `${name}`,
+              epost: `${dataEpost}`,
+              firmakode: `${code + code1 + code2 + code3}`,
+              admin: `${false}`,
+              aktiv: `${true}`,
+            });
+            setCompanies([]);
+            navigate("/nonprofits");
+          })
+          .catch((error) => {
+            setError(error.message);
+          });
+      } else {
+        setCodeError(true);
+        setCodeHelperText("Invalid code");
+      }
+    }
+  };
 
-	/* HandleOrgCode function */
-	function handleOrgCode(e) {
-		const value = e.target.value;
-		setCode(value);
-	}
-	useEffect(() => {
-		fetchCompanies();
-	}, []);
+  /* HandleOrgCode function */
+  function handleOrgCode(e) {
+    const value = e.target.value;
+    setCode(value);
+  }
 
-	return (
-		<div className="container-login">
-			<div className="container-left">
-				<h1 className="header-left">40 curated non-profits, to give you a competetive edge</h1>
-			</div>
-			<div className="container-right">
-				<img className="logo-login" src="./img/MI-logo-black1.png" alt="logo-pic-login" />
-				<div className="right-content">
-					<div className="right-content-header">
-						<h1>Sign up to Meliora Impact</h1>
-						<p className="header-p-inline">Already got a account?</p>
-						<Link to="/"> Log in</Link>
-					</div>
+  function handleOrgCode1(e) {
+    const value = e.target.value;
+    setCode1(value);
+  }
 
-					<div className="right-content-signin">
-						<h3>Enter Organization code</h3>
-						<div className="org-number">
-							<TextField
-								className="org-number-item"
-								variant="standard"
-								error={codeError}
-								helperText={codeHelperText}
-								onChange={handleOrgCode}
-							/>
-							<TextField className="org-number-item" variant="standard" error={codeError} />
-							<TextField className="org-number-item" variant="standard" error={codeError} />
-							<TextField className="org-number-item" variant="standard" error={codeError} />
-						</div>
+  function handleOrgCode2(e) {
+    const value = e.target.value;
+    setCode2(value);
+  }
 
-						<BlankInput
-							childToParent={childToParentEpost}
-							label="Navn"
-							error={nameError}
-							helperText={nameHelperText}
-							childToParent={childToParentName}
-						/>
-						<BlankInput
-							childToParent={childToParentEpost}
-							label="E-mail"
-							error={epostError}
-							helperText={epostHelperText}
-						/>
-						<PasswordInput
-							label="Create password"
-							childToParent={childToParentPassword}
-							error={passwordError}
-							helperText={passwordHelperText}
-						/>
-						<PasswordInput
-							label="Confirm password"
-							childToParent={childToParentPassword1}
-							error={passwordError1}
-							helperText={passwordHelperText1}
-						/>
-					</div>
-					<div className="right-content-button">
-						<Button size="large" color="secondary" variant="contained" onClick={handleLogin}>
-							Create account
-						</Button>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  function handleOrgCode3(e) {
+    const value = e.target.value;
+    setCode3(value);
+  }
+
+  useEffect(() => {
+    fetchCompanies();
+  }, []);
+
+  return (
+    <div className="container-login">
+      <div className="container-left">
+        <h1 className="header-left">
+          40 curated non-profits, to give you a competetive edge
+        </h1>
+      </div>
+      <div className="container-right">
+        <img
+          className="logo-login"
+          src="./img/MI-logo-black1.png"
+          alt="logo-pic-login"
+        />
+        <div className="right-content">
+          <div className="right-content-header">
+            <h1>Sign up to Meliora Impact</h1>
+            <p className="header-p-inline">Already got a account?</p>
+            <Link to="/"> Log in</Link>
+          </div>
+
+          <div className="right-content-signin">
+            <h3>Enter Organization code</h3>
+            <div className="org-number">
+              <TextField
+                className="org-number-item"
+                variant="standard"
+                error={codeError}
+                helperText={codeHelperText}
+                onChange={handleOrgCode}
+              />
+              <TextField
+                className="org-number-item"
+                variant="standard"
+                error={codeError}
+                onChange={handleOrgCode}
+              />
+              <TextField
+                className="org-number-item"
+                variant="standard"
+                error={codeError}
+                onChange={handleOrgCode}
+              />
+              <TextField
+                className="org-number-item"
+                variant="standard"
+                error={codeError}
+                onChange={handleOrgCode}
+              />
+            </div>
+
+            <BlankInput
+              childToParent={childToParentEpost}
+              label="Navn"
+              error={nameError}
+              helperText={nameHelperText}
+              childToParent={childToParentName}
+            />
+            <BlankInput
+              childToParent={childToParentEpost}
+              label="E-mail"
+              error={epostError}
+              helperText={epostHelperText}
+            />
+            <PasswordInput
+              label="Create password"
+              childToParent={childToParentPassword}
+              error={passwordError}
+              helperText={passwordHelperText}
+            />
+            <PasswordInput
+              label="Confirm password"
+              childToParent={childToParentPassword1}
+              error={passwordError1}
+              helperText={passwordHelperText1}
+            />
+          </div>
+          <div className="right-content-button">
+            <Button
+              size="large"
+              color="secondary"
+              variant="contained"
+              onClick={handleLogin}
+            >
+              Create account
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
