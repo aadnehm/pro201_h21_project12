6b52a074e983e1ee06ac2f0134ac7a02cdf19a18
terje <terjeslaatto@gmail.com>
2022-05-12 11:36:49 +0200

    updated data array

--- src/components/search/NonProfitsData.js
+++ src/components/search/NonProfitsData.js
@@ -1,49 +1,121 @@
 const nonProfitsPosts = [
-  {
-    id: "1",
-    img: "./img/eksempelbilde1.png",
-    name: "Redd barna",
-  },
-  {
-    id: "2",
-    img: "./img/eksempelbilde2.png",
-    name: "Charity:Water",
-  },
-  {
-    id: "3",
-    img: "./img/eksempelbilde3.png",
-    name: "Kreftforeningen",
-  },
-  {
-    id: "4",
-    img: "./img/eksempelbilde4.png",
-    name: "Occean Conservacy",
-  },
-  {
-    id: "5",
-    img: "./img/eksempelbilde5.png",
-    name: "Amnesty",
-  },
-  {
-    id: "6",
-    img: "./img/eksempelbilde6.png",
-    name: "Education Trust",
-  },
-  {
-    id: "7",
-    img: "./img/eksempelbilde7.png",
-    name: "The tomorrow school",
-  },
-  {
-    id: "8",
-    img: "./img/eksempelbilde8.png",
-    name: "Care Norge",
-  },
-  {
-    id: "9",
-    img: "./img/eksempelbilde9.png",
-    name: "Forum 18",
-  },
+	{
+		id             : '1',
+		img            : './img/eksempelbilde1.png',
+		name           : 'Redd barna',
+		averageDonated : 500,
+		aboutText      : 'lorem',
+		categories     : [
+			'recommended',
+			'education',
+			'environment',
+			'poverty'
+		]
+	},
+	{
+		id             : '2',
+		img            : './img/eksempelbilde2.png',
+		name           : 'Charity:Water',
+		averageDonated : 500,
+		aboutText      : 'lorem',
+		categories     : [
+			'recommended',
+			'education',
+			'environment',
+			'poverty'
+		]
+	},
+	{
+		id             : '3',
+		img            : './img/eksempelbilde3.png',
+		name           : 'Kreftforeningen',
+		averageDonated : 500,
+		aboutText      : 'lorem',
+		categories     : [
+			'recommended',
+			'education',
+			'environment',
+			'poverty'
+		]
+	},
+	{
+		id             : '4',
+		img            : './img/eksempelbilde4.png',
+		name           : 'Occean Conservacy',
+		averageDonated : 500,
+		aboutText      : 'lorem',
+		categories     : [
+			'recommended',
+			'education',
+			'environment',
+			'poverty'
+		]
+	},
+	{
+		id             : '5',
+		img            : './img/eksempelbilde5.png',
+		name           : 'Amnesty',
+		averageDonated : 500,
+		aboutText      : 'lorem',
+		categories     : [
+			'recommended',
+			'education',
+			'environment',
+			'poverty'
+		]
+	},
+	{
+		id             : '6',
+		img            : './img/eksempelbilde6.png',
+		name           : 'Education Trust',
+		averageDonated : 500,
+		aboutText      : 'lorem',
+		categories     : [
+			'recommended',
+			'education',
+			'environment',
+			'poverty'
+		]
+	},
+	{
+		id             : '7',
+		img            : './img/eksempelbilde7.png',
+		name           : 'The tomorrow school',
+		averageDonated : 500,
+		aboutText      : 'lorem',
+		categories     : [
+			'recommended',
+			'education',
+			'environment',
+			'poverty'
+		]
+	},
+	{
+		id             : '8',
+		img            : './img/eksempelbilde8.png',
+		name           : 'Care Norge',
+		averageDonated : 500,
+		aboutText      : 'lorem',
+		categories     : [
+			'recommended',
+			'education',
+			'environment',
+			'poverty'
+		]
+	},
+	{
+		id             : '9',
+		img            : './img/eksempelbilde9.png',
+		name           : 'Forum 18',
+		averageDonated : 500,
+		aboutText      : 'lorem',
+		categories     : [
+			'recommended',
+			'education',
+			'environment',
+			'poverty'
+		]
+	}
 ];
 
 export default nonProfitsPosts;
