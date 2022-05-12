dde81709427d81b325a292807008ad79c16dd77e
aadne <adma007@student.kristiania.no>
2022-05-12 11:48:58 +0200

    test

---
+++ src/components/navbar/Menu.js
@@ -0,0 +1,13 @@
+import "./Navbar.css";
+import React from "react";
+import { useNavigate } from "react-router";
+
+class Menu extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Yo</h1>
+      </div>
+    );
+  }
+}
