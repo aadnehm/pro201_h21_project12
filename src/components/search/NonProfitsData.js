7b135ab46a0cea5a9f9dbc2d2d273978d18a4724
SigmundGjengedal <sigmund.gjengedal@gmail.com>
2022-05-12 14:48:04 +0200

    changed NonprofitsData

--- src/components/search/NonProfitsData.js
+++ src/components/search/NonProfitsData.js
@@ -1,121 +1,76 @@
 const nonProfitsPosts = [
-	{
-		id             : '1',
-		img            : './img/eksempelbilde1.png',
-		name           : 'Redd barna',
-		averageDonated : 500,
-		aboutText      : 'lorem',
-		categories     : [
-			'recommended',
-			'education',
-			'environment',
-			'poverty'
-		]
-	},
-	{
-		id             : '2',
-		img            : './img/eksempelbilde2.png',
-		name           : 'Charity:Water',
-		averageDonated : 500,
-		aboutText      : 'lorem',
-		categories     : [
-			'recommended',
-			'education',
-			'environment',
-			'poverty'
-		]
-	},
-	{
-		id             : '3',
-		img            : './img/eksempelbilde3.png',
-		name           : 'Kreftforeningen',
-		averageDonated : 500,
-		aboutText      : 'lorem',
-		categories     : [
-			'recommended',
-			'education',
-			'environment',
-			'poverty'
-		]
-	},
-	{
-		id             : '4',
-		img            : './img/eksempelbilde4.png',
-		name           : 'Occean Conservacy',
-		averageDonated : 500,
-		aboutText      : 'lorem',
-		categories     : [
-			'recommended',
-			'education',
-			'environment',
-			'poverty'
-		]
-	},
-	{
-		id             : '5',
-		img            : './img/eksempelbilde5.png',
-		name           : 'Amnesty',
-		averageDonated : 500,
-		aboutText      : 'lorem',
-		categories     : [
-			'recommended',
-			'education',
-			'environment',
-			'poverty'
-		]
-	},
-	{
-		id             : '6',
-		img            : './img/eksempelbilde6.png',
-		name           : 'Education Trust',
-		averageDonated : 500,
-		aboutText      : 'lorem',
-		categories     : [
-			'recommended',
-			'education',
-			'environment',
-			'poverty'
-		]
-	},
-	{
-		id             : '7',
-		img            : './img/eksempelbilde7.png',
-		name           : 'The tomorrow school',
-		averageDonated : 500,
-		aboutText      : 'lorem',
-		categories     : [
-			'recommended',
-			'education',
-			'environment',
-			'poverty'
-		]
-	},
-	{
-		id             : '8',
-		img            : './img/eksempelbilde8.png',
-		name           : 'Care Norge',
-		averageDonated : 500,
-		aboutText      : 'lorem',
-		categories     : [
-			'recommended',
-			'education',
-			'environment',
-			'poverty'
-		]
-	},
-	{
-		id             : '9',
-		img            : './img/eksempelbilde9.png',
-		name           : 'Forum 18',
-		averageDonated : 500,
-		aboutText      : 'lorem',
-		categories     : [
-			'recommended',
-			'education',
-			'environment',
-			'poverty'
-		]
-	}
+  {
+    id: "1",
+    img: "./img/eksempelbilde1.png",
+    name: "Redd barna",
+    averageDonated: 300,
+    aboutText: "lorem ipsum cadeva ivo fiva pova ipsum",
+    categories: "education",
+  },
+  {
+    id: "2",
+    img: "./img/eksempelbilde2.png",
+    name: "Charity:Water",
+    averageDonated: 200,
+    aboutText: "lorem ipsum cadeva ivo fiva pova lorem ",
+    categories: "environment",
+  },
+  {
+    id: "3",
+    img: "./img/eksempelbilde3.png",
+    name: "Kreftforeningen",
+    averageDonated: 150,
+    aboutText: "lorem ipsum cadeva ivo fiva pova",
+    categories: "poverty",
+  },
+  {
+    id: "4",
+    img: "./img/eksempelbilde4.png",
+    name: "Occean Conservacy",
+    averageDonated: 300,
+    aboutText: "lorem ipsum cadeva ivo fiva pova",
+    categories: "education",
+  },
+  {
+    id: "5",
+    img: "./img/eksempelbilde5.png",
+    name: "Amnesty",
+    averageDonated: 350,
+    aboutText: "lorem ipsum cadeva ivo fiva pova",
+    categories: "poverty",
+  },
+  {
+    id: "6",
+    img: "./img/eksempelbilde6.png",
+    name: "Education Trust",
+    averageDonated: 400,
+    aboutText: "lorem ipsum cadeva ivo fiva pova",
+    categories: "humanitarian",
+  },
+  {
+    id: "7",
+    img: "./img/eksempelbilde7.png",
+    name: "The tomorrow school",
+    averageDonated: 200,
+    aboutText: "lorem ipsum cadeva ivo fiva pova",
+    categories: "poverty",
+  },
+  {
+    id: "8",
+    img: "./img/eksempelbilde8.png",
+    name: "Care Norge",
+    averageDonated: 300,
+    aboutText: "lorem ipsum cadeva ivo fiva pova",
+    categories: "education",
+  },
+  {
+    id: "9",
+    img: "./img/eksempelbilde9.png",
+    name: "Forum 18",
+    averageDonated: 200,
+    aboutText: "lorem ipsum cadeva ivo fiva pova",
+    categories: "poverty",
+  },
 ];
 
 export default nonProfitsPosts;
