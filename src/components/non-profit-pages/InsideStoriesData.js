735f68fcc2f3116e7e5d9ca7a80814b3295dbb1e
aadne <adma007@student.kristiania.no>
2022-05-20 12:05:16 +0200

    inside stories is so pretty now

--- src/components/non-profit-pages/InsideStoriesData.js
+++ src/components/non-profit-pages/InsideStoriesData.js
@@ -3,7 +3,9 @@ const insideStoriesData = [
     id: "1",
     link: "https://youtu.be/dQw4w9WgXcQ",
     title: "Michael Stevens",
-    about: "Not actually Michael Stevens",
+    about: "This is Michael Stevens video on love. A philosophy.",
+    longText:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae, sit. Architecto minus temporibus quasi, ipsum accusamus dolores tempore, omnis modi, ex placeat nemo nulla expedita optio et facilis obcaecati perspiciatis.",
   },
 
   {
@@ -11,6 +13,8 @@ const insideStoriesData = [
     link: "https://www.youtube.com/watch?v=U6lpBi39N78",
     title: "Torstein Horgmo",
     about: "Horgasm - A Love Story",
+    longText:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque eaque amet harum magnam architecto, odio aliquid ut incidunt ea quas neque corrupti omnis dolorum. Asperiores ea quia nesciunt, dignissimos eaque quae necessitatibus ducimus modi distinctio earum aut rerum architecto voluptatibus tenetur sapiente sunt illum cupiditate iusto deserunt inventore optio rem! Quam omnis odit totam, maiores voluptate sunt numquam dolore eligendi sint nam pariatur exercitationem perferendis ipsa voluptates cupiditate molestias iste impedit iure itaque? Consectetur corporis est ipsum sapiente laborum, officiis quaerat omnis quidem provident ullam natus sunt distinctio explicabo eos quisquam sed. Cum tempore illum similique quisquam magnam voluptatibus voluptatem.",
   },
 
   {
@@ -18,20 +22,26 @@ const insideStoriesData = [
     link: "https://youtu.be/dQw4w9WgXcQ",
     title: "Rick Astley",
     about: "Get rickrolled boiii",
+    longText:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque eaque amet harum magnam architecto, odio aliquid ut incidunt ea quas neque corrupti omnis dolorum. Asperiores ea quia nesciunt, dignissimos eaque quae necessitatibus ducimus modi distinctio earum aut rerum architecto voluptatibus tenetur sapiente sunt illum cupiditate iusto deserunt inventore optio rem! Quam omnis odit totam, maiores voluptate sunt numquam dolore eligendi sint nam pariatur exercitationem perferendis ipsa voluptates cupiditate molestias iste impedit iure itaque? Consectetur corporis est ipsum sapiente laborum, officiis quaerat omnis quidem provident ullam natus sunt distinctio explicabo eos quisquam sed. Cum tempore illum similique quisquam magnam voluptatibus voluptatem.",
   },
 
   {
     id: "4",
     link: "https://www.youtube.com/watch?v=U6lpBi39N78",
     title: "Michael Stevens",
-    about: "Still not actually Michael Stevens",
+    about: "Hello",
+    longText:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque eaque amet harum magnam architecto, odio aliquid ut incidunt ea quas neque corrupti omnis dolorum. Asperiores ea quia nesciunt, dignissimos eaque quae necessitatibus ducimus modi distinctio earum aut rerum architecto voluptatibus tenetur sapiente sunt illum cupiditate iusto deserunt inventore optio rem! Quam omnis odit totam, maiores voluptate sunt numquam dolore eligendi sint nam pariatur exercitationem perferendis ipsa voluptates cupiditate molestias iste impedit iure itaque? Consectetur corporis est ipsum sapiente laborum, officiis quaerat omnis quidem provident ullam natus sunt distinctio explicabo eos quisquam sed. Cum tempore illum similique quisquam magnam voluptatibus voluptatem.",
   },
 
   {
     id: "5",
     link: "https://www.youtube.com/watch?v=U6lpBi39N78",
     title: "Michael Stevens",
-    about: "Still not actually Michael Stevens",
+    about: "Not actually Michael Stevens",
+    longText:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque eaque amet harum magnam architecto, odio aliquid ut incidunt ea quas neque corrupti omnis dolorum. Asperiores ea quia nesciunt, dignissimos eaque quae necessitatibus ducimus modi distinctio earum aut rerum architecto voluptatibus tenetur sapiente sunt illum cupiditate iusto deserunt inventore optio rem! Quam omnis odit totam, maiores voluptate sunt numquam dolore eligendi sint nam pariatur exercitationem perferendis ipsa voluptates cupiditate molestias iste impedit iure itaque? Consectetur corporis est ipsum sapiente laborum, officiis quaerat omnis quidem provident ullam natus sunt distinctio explicabo eos quisquam sed. Cum tempore illum similique quisquam magnam voluptatibus voluptatem.",
   },
 
   {
@@ -39,6 +49,8 @@ const insideStoriesData = [
     link: "https://www.youtube.com/watch?v=U6lpBi39N78",
     title: "Michael Stevens",
     about: "Still not actually Michael Stevens",
+    longText:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque eaque amet harum magnam architecto, odio aliquid ut incidunt ea quas neque corrupti omnis dolorum. Asperiores ea quia nesciunt, dignissimos eaque quae necessitatibus ducimus modi distinctio earum aut rerum architecto voluptatibus tenetur sapiente sunt illum cupiditate iusto deserunt inventore optio rem! Quam omnis odit totam, maiores voluptate sunt numquam dolore eligendi sint nam pariatur exercitationem perferendis ipsa voluptates cupiditate molestias iste impedit iure itaque? Consectetur corporis est ipsum sapiente laborum, officiis quaerat omnis quidem provident ullam natus sunt distinctio explicabo eos quisquam sed. Cum tempore illum similique quisquam magnam voluptatibus voluptatem.",
   },
 
   {
@@ -46,6 +58,8 @@ const insideStoriesData = [
     link: "https://www.youtube.com/watch?v=U6lpBi39N78",
     title: "Michael Stevens",
     about: "Still not actually Michael Stevens",
+    longText:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque eaque amet harum magnam architecto, odio aliquid ut incidunt ea quas neque corrupti omnis dolorum. Asperiores ea quia nesciunt, dignissimos eaque quae necessitatibus ducimus modi distinctio earum aut rerum architecto voluptatibus tenetur sapiente sunt illum cupiditate iusto deserunt inventore optio rem! Quam omnis odit totam, maiores voluptate sunt numquam dolore eligendi sint nam pariatur exercitationem perferendis ipsa voluptates cupiditate molestias iste impedit iure itaque? Consectetur corporis est ipsum sapiente laborum, officiis quaerat omnis quidem provident ullam natus sunt distinctio explicabo eos quisquam sed. Cum tempore illum similique quisquam magnam voluptatibus voluptatem.",
   },
 
   {
@@ -53,6 +67,8 @@ const insideStoriesData = [
     link: "https://www.youtube.com/watch?v=U6lpBi39N78",
     title: "Michael Stevens",
     about: "Still not actually Michael Stevens",
+    longText:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque eaque amet harum magnam architecto, odio aliquid ut incidunt ea quas neque corrupti omnis dolorum. Asperiores ea quia nesciunt, dignissimos eaque quae necessitatibus ducimus modi distinctio earum aut rerum architecto voluptatibus tenetur sapiente sunt illum cupiditate iusto deserunt inventore optio rem! Quam omnis odit totam, maiores voluptate sunt numquam dolore eligendi sint nam pariatur exercitationem perferendis ipsa voluptates cupiditate molestias iste impedit iure itaque? Consectetur corporis est ipsum sapiente laborum, officiis quaerat omnis quidem provident ullam natus sunt distinctio explicabo eos quisquam sed. Cum tempore illum similique quisquam magnam voluptatibus voluptatem.",
   },
 ];
 
