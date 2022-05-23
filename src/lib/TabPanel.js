8b1c30c29579622c4455bcf862ed11579a84e416
andersholt <anho068@student.kristiania.no>
2022-05-23 13:22:15 +0200

    Refactor names

---
+++ src/lib/TabPanel.js
@@ -0,0 +1,29 @@
+import Box from "@mui/material/Box";
+import PropTypes from "prop-types";
+import React from "react";
+
+export default function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ width: "100%" }}>
+          <div>{children}</div>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
