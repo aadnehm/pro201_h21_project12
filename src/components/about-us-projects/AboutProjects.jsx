f695075817b8e320b2a01da974e207197fa92cac
SigmundGjengedal <sigmund.gjengedal@gmail.com>
2022-05-25 21:48:07 +0200

    testemonials v1

--- src/components/about-us-projects/AboutProjects.jsx
+++ src/components/about-us-projects/AboutProjects.jsx
@@ -5,7 +5,7 @@ import TimeLine from "./TimeLine";
 export default function AboutProjects({ project }) {
   return (
     <>
-      <div className="about-info-container">
+      <div className="about-component-container">
         <h2>About {project.name}</h2>
         <div
           className="img-container"
@@ -27,6 +27,58 @@ export default function AboutProjects({ project }) {
         <h2>Our progress</h2>
         <div className="timeline-wrapper">
           <TimeLine />
+        </div>
+        <div className="testomonials">
+          <h2>Testemonials</h2>
+          <p className="p-margin">
+            Our donators are companies that want to make a difference for
+            vulnerable children. Together, we invest in children's rights, which
+            ensure a sustainable future. Read about how they have contributed to
+            a better world.
+          </p>
+        </div>
+        <div className="companies-testemonials">
+          <div>
+            <h4>Foodora</h4>
+            <p>
+              The collaboration with Save the Children is well established among
+              Foodora`s committed employees. A large proportion of the employees
+              at Foodora Norway give a fixed amount to Save the Children every
+              month through payroll. This money goes to Project Somalia. We have
+              seen that our employees take greater pride in working for Foodora.
+            </p>
+          </div>
+          <div>
+            <h4>Avinor</h4>
+            <p>
+              The collaboration with Save the Children is well established among
+              Avinor`s committed employees. A large proportion of the employees
+              at Avinor give a fixed amount to Save the Children every month
+              through payroll. This money goes to Project Somalia. We have seen
+              that our employees take greater pride in working for Foodora.
+            </p>
+          </div>
+          <div>
+            <h4>Columbus</h4>
+            <p>
+              The collaboration with Save the Children is well established among
+              Colombus committed employees. A large proportion of the employees
+              at Columbus give a fixed amount to Save the Children every month
+              through payroll. This money goes to Project Somalia. We have seen
+              that our employees take greater pride in working for Foodora.
+            </p>
+          </div>
+          <div>
+            <h4>DNB Bank</h4>
+            <p>
+              The collaboration with Save the Children is well established among
+              DNB Bank`s committed employees. A large proportion of the
+              employees at DNB Bank give a fixed amount to Save the Children
+              every month through payroll. This money goes to Project Somalia.
+              We have seen that our employees take greater pride in working for
+              Foodora.
+            </p>
+          </div>
         </div>
       </div>
     </>
